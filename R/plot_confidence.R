e257ce883de39765d03432223f6f89c2cef995d3
seabbs <s.e.abbott12@gmail.com>
2020-03-23 19:14:08 +0000

    tweaked plots

--- R/plot_confidence.R
+++ R/plot_confidence.R
@@ -31,7 +31,7 @@ plot_confidence <- function(data, outer_alpha = 0.1, inner_alpha = 0.2, plot_med
     ggplot2::scale_alpha(range = c(0, 0.5)) +
     cowplot::theme_cowplot() +
     ggplot2::scale_x_date(date_breaks = "1 week", date_labels = "%b %d") +
-    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45))
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90))
 
 
   ## Confident ribbons
@@ -62,10 +62,10 @@ plot_confidence <- function(data, outer_alpha = 0.1, inner_alpha = 0.2, plot_med
       plot <- plot +
         ggplot2::geom_ribbon(data = varying_conf_data[seq(i - 1, i),],
                              ggplot2::aes(ymin = bottom, ymax = top),
-                             col = "#344b85",
+                             fill = "#344b85",
                              alpha = varying_conf_data$confidence[i] * outer_alpha) +
         ggplot2::geom_ribbon(data = varying_conf_data[seq(i - 1, i),],
-                             col = "#344b85",
+                             fill = "#344b85",
                              ggplot2::aes(ymin = lower, ymax = upper),
                              alpha = varying_conf_data$confidence[i] * inner_alpha)
 
