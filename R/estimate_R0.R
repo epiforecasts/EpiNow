f7c9f4c9c07552ccc044c629282f5360fc9ae4ee
seabbs <s.e.abbott12@gmail.com>
2020-03-28 21:35:46 +0000

    add forecasting to estimate_R0

--- R/estimate_R0.R
+++ R/estimate_R0.R
@@ -7,11 +7,12 @@
 #' @param rt_prior A list defining the reproduction number prior containing the mean (`mean_prior`) and standard deviation (`std_prior`)
 #' @param windows Numeric vector, windows over which to estimate time-varying R. The best performing window will be 
 #' selected per serial interval sample by default (based on which window best forecasts current cases). 
-#' @param return_best Logical defaults to `TRUE`. Should the estimates for the best performing window be returned or estimates 
-#' for all windows.
 #' @param si_samples Numeric, the number of samples to take from the serial intervals supplied
 #' @param rt_samples Numeric, the number of samples to take from the estimated R distribution for each time point.
 #' @param min_est_date Date to begin estimation.
+#' @param forecast_model An uninitialised bsts model passed to `EpiSoon::forecast_rt` to be used for forecasting
+#' future Rt values. An example of the required structure is: `function(ss, y){bsts::AddSemilocalLinearTrend(ss, y = y)}`.
+#' @param horizon Numeric, defaults to 0. The horizon over which to forecast Rts and cases.
 #' @inheritParams add_dates
 #' @return A tibble containing the date and summarised R estimte.
 #' @export
@@ -20,30 +21,33 @@
 #' @importFrom dplyr rename full_join pull
 #' @importFrom tibble tibble
 #' @importFrom purrr map2
+#' @importFrom EpiSoon predict_current_cases forecast_rt forecast_cases score_case_forecast
 #' @examples
 #'
-#' cases <- data.frame(date = seq(as.Date("2020-01-01"),
-#'                                as.Date("2020-01-30"),
-#'                                by = "days"),
-#'                     cases = 1:30)
 #'                     
-#'                     
-#' serial_intervals <- as.matrix(EpiNow::covid_serial_intervals[,1])
-#' rt_prior <- list(mean_prior = 2.6, std_prior = 2) 
-#' windows <- c(1, 3, 7)
+#' estimates <- estimate_R0(cases = EpiSoon::example_obs_cases, 
+#'                          serial_intervals = as.matrix(EpiNow::covid_serial_intervals[,1]), 
+#'                          rt_prior = list(mean_prior = 2.6, std_prior = 2),
+#'                          windows = c(1, 3, 7), rt_samples = 10, si_samples = 2,
+#'                          min_est_date =  as.Date("2020-02-18"),
+#'                          forecast_model = function(ss, y){bsts::AddSemilocalLinearTrend(ss, y = y)},
+#'                          horizon = 7)
+#'                          
+#'## Rt estimates and forecasts
+#' estimates$rts
 #' 
-#' estimate_R0(cases, serial_intervals, 
-#'             rt_prior = rt_prior, windows = windows,
-#'             rt_samples = 10, si_samples = 2, return_best = FALSE,
-#'             min_est_date =  as.Date("2020-01-01"))
+#' 
+#' ## Case forecasts
+#' estimates$cases
 estimate_R0 <- function(cases = NULL, serial_intervals = NULL,
                         rt_prior = NULL, windows = NULL, 
                         si_samples = 100, rt_samples = 100,
-                        return_best = TRUE, min_est_date = NULL) {
+                        min_est_date = NULL, forecast_model = NULL, 
+                        horizon = 0) {
  
  
   ## Adjust input based on the presence of imported cases
-  if (length(unique(cases$import_status)) > 1) {
+  if (suppressWarnings(length(unique(cases$import_status)) > 1)) {
     incid <- cases %>%
       dplyr::select(date, cases, import_status) %>% 
       tidyr::spread(key = "import_status", value = "cases") %>%
@@ -89,7 +93,8 @@ estimate_R0 <- function(cases = NULL, serial_intervals = NULL,
 
    
   ### Estimate R across serial interval samples
-  est_r <- purrr::map_dfr(serial_intervals_index, function(index) {
+  ### Forecast ahead if given a model and horizon for each sample
+  estimates <- purrr::map(serial_intervals_index, function(index) {
     
     ### Estimate and score R over multiple windows
     est_r <- purrr::map_dfr(windows, 
@@ -165,56 +170,79 @@ estimate_R0 <- function(cases = NULL, serial_intervals = NULL,
                           return(out)
                         })
     
-    ## Return the best performing window
-    if (return_best) {
+    ## Select the best performing window
       est_r <- est_r %>% 
         dplyr::filter(score == min(score)) %>% 
         dplyr::filter(window == min(window)) %>% 
         dplyr::select(-score, -score_sd)
-    }
-
-    
-    model <- function(ss, y){bsts::AddSemilocalLinearTrend(ss, y = y)}
-    horizon <- 21
-    
-    
-    ## Forecast Rts
-    rt_forecasts <- est_r %>% 
-      dplyr::select(date, rt = R, sample) %>% 
-      dplyr::group_split(sample) %>% 
-      purrr::map(
-        ~ EpiSoon::forecast_rt(.,
-                             model = model, 
-                             horizon = horizon,
-                             samples = 1) %>% 
-          dplyr::select(-sample))
-    
-    ## Bind Rts forecasts together
-    rt_forecasts <- dplyr::bind_rows(rt_forecasts, .id = "sample") %>% 
-      dplyr::mutate(sample = as.numeric(sample))
-    
-    ## Forecast cases
-    case_forecasts <-   
-      EpiSoon::forecast_cases(
-          cases = summed_cases,
-          fit_samples = rt_forecasts,
-          rdist = rpois,
-          serial_interval = serial_intervals[, index]
-        )
+      
+      
+      
+      if (horizon > 0 & !is.null(forecast_model)) {
+        ## Forecast Rts
+        rt_forecasts <- est_r %>% 
+          dplyr::select(date, rt = R, sample) %>% 
+          dplyr::group_split(sample) %>% 
+          purrr::map(
+            ~ EpiSoon::forecast_rt(.,
+                                   model = model, 
+                                   horizon = horizon,
+                                   samples = 1) %>% 
+              dplyr::select(-sample))
+        
+        ## Bind Rts forecasts together
+        rt_forecasts <- dplyr::bind_rows(rt_forecasts, .id = "sample") %>% 
+          dplyr::mutate(sample = as.numeric(sample))
+        
+        ## Forecast cases
+        case_forecasts <-   
+          EpiSoon::forecast_cases(
+            cases = summed_cases,
+            fit_samples = rt_forecasts,
+            rdist = rpois,
+            serial_interval = serial_intervals[, index]
+          ) %>% 
+          dplyr::mutate(type = "forecast")
+        
+        
+        ## Join Rt estimates and forecasts
+        est_r <- est_r %>% 
+          dplyr::mutate(type = "nowcast") %>% 
+          dplyr::bind_rows(rt_forecasts %>% 
+                             dplyr::mutate(type = "forecast") %>% 
+                             dplyr::select(date, R = rt, sample,  horizon, type))
+        
+        return(list(rts = est_r, cases = case_forecasts))
+        
+      }else{
+        
+        ## Return just nowcast if no forecast has been run
+        est_r <- est_r %>% 
+          dplyr::mutate(type = "nowcast", horizon = NA)
+        
+        return(list(rts = est_r, cases = data.frame()))
+      }
+      
+  })
   
-    
-   return(est_r) 
-  }, .id = "si_sample")
-  
+  ## Reorganise list output
+  estimates <- purrr::transpose(estimates)
 
+
+  
+  ## Organise returns to have a unique sample ID
   if (si_samples == 1) {
-    return(est_r)
+    return(estimates)
   }else{
-    est_r <- dplyr::mutate(est_r, sample = sample * as.numeric(si_sample)) %>% 
-      dplyr::select(-si_sample)
     
-    return(est_r)
-}
+    estimates <- estimates %>% 
+      purrr::map( ~ dplyr::bind_rows(., .id = "si_sample") %>% 
+                    dplyr::mutate(sample = sample * as.numeric(si_sample)) %>% 
+                    dplyr::select(-si_sample)
+      )
+    
+    return(estimates)
+  }
 
 }
 
