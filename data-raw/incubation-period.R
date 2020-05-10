5681989649e5e656f4718067f2ff94126a3650e6
seabbs <s.e.abbott12@gmail.com>
2020-05-10 12:56:53 +0000

    sourced incubation period

--- data-raw/incubation-period.R
+++ data-raw/incubation-period.R
@@ -1,13 +1,11 @@
 library(data.table)
 
-## Parameters values sourced from: 
-## * https://annals.org/aim/fullarticle/2762808/incubation-period-coronavirus-disease-2019-covid-19-from-publicly-reported
-
 covid_incubation_period <- data.table(
   as_reported = "5.06 (log SD 0.418)",
   mean = 1.621, mean_sd = 0.0640, 
   sd = 0.418, sd_sd = 0.0691,
-  source = "lauer"
+  source = "lauer",
+  url = "doi.org/10.7326/M20-0504"
 )
 
 usethis::use_data(covid_incubation_period, overwrite = TRUE)
